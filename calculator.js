35eda81aab2bc40ddc80029d7941b67382d2803e
YoannaKuneva <79211246+YoannaKuneva@users.noreply.github.com>
2021-02-27 15:45:45 +0200

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,36 @@
+// 16 зад.
+function calculate([a, b, op]) {
+    [a, b] = [a, b].map(Number);
+
+    let add = function (a, b) {
+        return a + b;
+    }
+
+    let substract = function (a, b) {
+        return a - b;
+    }
+
+    let multiply = function (a, b) {
+        return a * b;
+    }
+    
+    let devide = function (a, b) {
+        return a / b;
+    }
+
+    let calc = function (a, b, op) {
+        return op (a, b);
+    }
+
+    switch(op) {
+        case '+': return calc (a, b, add);
+        case '-': return calc (a, b, substract);
+        case '*': return calc (a, b, multiply);
+        case '/': return calc (a, b, devide);
+    }
+}
+
+console.log(calculate(['8', '2', '+']));
+console.log(calculate(['8', '2', '-']));
+console.log(calculate(['8', '2', '*']));
+console.log(calculate(['8', '2', '/']));
