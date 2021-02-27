c94b0bea210cda0bb96e9b21c85f848157c6c356
YoannaKuneva <79211246+YoannaKuneva@users.noreply.github.com>
2021-02-27 15:39:03 +0200

    Create boxes.js

---
+++ boxes.js
@@ -0,0 +1,9 @@
+// 8 зад.
+function boxesAndBottles(bottles, capacity) {
+    let result = Math.ceil(bottles / capacity);
+    console.log(result);
+}
+
+boxesAndBottles(20, 5);
+boxesAndBottles(15, 7);
+boxesAndBottles(5, 10);
