003d369be4210854bef7f61c07173c006b8205e7
YoannaKuneva <79211246+YoannaKuneva@users.noreply.github.com>
2021-02-27 15:37:37 +0200

    Create stringnums.js

---
+++ stringnums.js
@@ -0,0 +1,11 @@
+// 6 зад.
+function stringOfNumbers(lastNum) {
+    let result = '';
+    for (let i = 1; i <= lastNum; i++) {
+        result += i;
+    }
+    console.log(result);
+}
+
+stringOfNumbers(10);
+stringOfNumbers(20);
